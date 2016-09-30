a15a528ff1de203d18c1703d036bf4b2a52d1fe2
Biencis Rubiera <biencis@gmail.com>
2016-09-30 17:55:36 +0000

    Created module task_02 and testtask02 to test it

---
+++ task_02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Practicing importing a module with its full Namespace"""
+
+import task_01.peanut
+
+TIME = task_01.peanut.BUTTER
---
+++ testtask02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Testing module task_02"""
+
+import task_02
+
+if task_02.TIME: print 'I am truthy too!'
