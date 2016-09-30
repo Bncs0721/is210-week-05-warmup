43ec4365f573d16b0ba9acd6b03e36b60535aeb7
Biencis Rubiera <biencis@gmail.com>
2016-09-30 18:29:49 +0000

    Created module task_03.py and testtask03.py to test it

---
+++ task_03.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8  -*-
+"""Practicing to copy a module attribute"""
+
+from task_01.peanut import BUTTER
+
+JELLY = BUTTER
+
+# Alternative: import task_01.peanut task_01.peanut.BUTTER = JELLY
---
+++ testtask03.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Testing task_03"""
+
+import task_03
+
+print True if task_03.BUTTER else False
+
+print task_03.JELLY == task_03.BUTTER
+
+
