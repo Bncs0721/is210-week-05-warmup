686be26da9d812e5185bc1e600d6aa5946d2c52f
Biencis Rubiera <biencis@gmail.com>
2016-09-30 17:47:01 +0000

    Created package task_01 and module peanut, then created peanuttest to test it

---
+++ peanuttest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Testing peanut module"""
+
+import task_01.peanut
+
+if task_01.peanut.BUTTER: print 'I am Truthy'
+
+if not task_01.peanut.OIL: print 'I am Falsy'
---
+++ task_01/peanut.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Creating constants BUTTER and OIL"""
+
+BUTTER = 1
+
+OIL = 0
